8c0a3765c75eff0c1dba9d2d32f10e1ef4a17f63
howardhunters <154858545+howardhunters@users.noreply.github.com>
2024-06-29 16:17:34 +1200

    Update index.js

--- files/index.js
+++ files/index.js
@@ -5,9 +5,9 @@ const port = process.env.SERVER_PORT || process.env.PORT || 3000;
 const UUID = process.env.UUID || '986e0d08-b275-4dd3-9e75-f3094b36fa2a'; //若需要改UUID，需要在config.json里改为一致
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'koyeb.uuidkuy.com';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '443';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
-const NEZHA_KEY = process.env.NEZHA_KEY || 'CEpUSHYvLxriPizBzW';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo.uuy-yydss.bf';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMDhmNDA4NjVlNzljMDNhMjE4YjQwYzYwYmQyYjQ0OTEiLCJ0IjoiMjc4ZTk0NmUtMjc0Ny00OWM4LThjZjQtZjk5MmY3MzIyYmI3IiwicyI6Ik5XRXlPR0UyTkRZdE5HRm1ZaTAwTXpRM0xUa3dNelV0WmpNek5HUTBOR0ptTWpneiJ9';
+const NEZHA_KEY = process.env.NEZHA_KEY || '9ZCgZTq6Gyh0jp6OtZ';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreoceshi.uuy-yydss.bf';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMDhmNDA4NjVlNzljMDNhMjE4YjQwYzYwYmQyYjQ0OTEiLCJ0IjoiZDgwZTVmZTYtODAyZS00MzFhLWJlN2ItOGQyNTkyYjJmOWE3IiwicyI6Ik4yTmxOVEptWlRZdE56YzFOUzAwT1dGbExUbGhZalV0TW1ObVlXTmpNbVptTlRSaCJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
